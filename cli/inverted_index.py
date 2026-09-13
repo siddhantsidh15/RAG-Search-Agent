71b36eae5c7b97ade16038b9ed7fda37fc896f7c
siddhantsiddh15 <siddhant.bitm020@gmail.com>
2026-09-13 16:13:45 +0530

    Term frequencies

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,6 +1,6 @@
 
 import pickle
-from collections import defaultdict
+from collections import defaultdict, Counter
 import os
 
 from utils import CACHE_DIR, tokenize_text, load_movies
@@ -11,11 +11,17 @@ class InvertedIndex:
         self.docmap: dict[int, dict] = {}
         self.index_path = CACHE_DIR / "index.pkl"
         self.docmap_path = CACHE_DIR / "docmap.pkl"
+        self.tf_path = CACHE_DIR / "term_frequencies.pkl"
+        self.term_frequencies = defaultdict(Counter)
 
     def __add_document(self, doc_id : int, text : str) -> None:
         tokens = tokenize_text(text)
         for token in set(tokens):
             self.index[token].add(doc_id)
+        self.term_frequencies[doc_id].update(tokens)
+
+    def get_tf(self, doc_id: int, term: str) -> int:
+        return self.term_frequencies[doc_id][term]
 
     def get_documents(self, term) -> list[int]:
         return sorted(self.index.get(term, set()))
@@ -34,5 +40,60 @@ class InvertedIndex:
             pickle.dump(self.index, f)
         with open(self.docmap_path, "wb") as f:
             pickle.dump(self.docmap, f)
+        with open(self.tf_path, "wb") as f:
+            pickle.dump(self.term_frequencies, f)
+
+    def load(self) -> None:
+
+        try:
+            with open(self.index_path, "rb") as f:
+                self.index = pickle.load(f)
+            with open(self.docmap_path, "rb") as f:
+                self.docmap = pickle.load(f)
+            with open(self.tf_path, "rb") as f:
+                self.term_frequencies = pickle.load(f)
+        except FileNotFoundError:
+            pass
         
 
+
+def search_command(QUERY: str, limit : int) -> list[dict]:
+
+    try :
+        idx = InvertedIndex()
+        idx.load()
+        # -----------
+        query_tokens = tokenize_text(QUERY)
+
+        seen, results = set(), []
+        for query_token in query_tokens:
+            matching_doc_ids = idx.get_documents(query_token)
+            for doc_id in matching_doc_ids:
+                if doc_id in seen:
+                    continue
+                seen.add(doc_id)
+                doc = idx.docmap[doc_id]
+                results.append(doc)
+                if len(results) >= limit:
+                    return results
+        return results
+    except FileNotFoundError:
+        pass
+
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
+def tokenize_single_term(term: str) -> str:
+    tokens = tokenize_text(term)
+    if len(tokens) != 1:
+        raise ValueError("term must be a single token")
+    return tokens[0]
+
+
+def tf_command(doc_id: int, term: str) -> int:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf(doc_id, tokenize_single_term(term))
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,8 @@
 import argparse
 import string
 
-from utils import load_movies,load_stop_words,check_term
-from inverted_index import InvertedIndex
+from utils import tokenize_text,check_term
+from inverted_index import build_command, tf_command, search_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -10,40 +10,32 @@ def main() -> None:
 
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
     search_parser.add_argument("query", type=str, help="Search query")
-    build_parser = subparsers.add_parser("build", help="Build inverted index")
+
+    build_parse = subparsers.add_parser("build", help="Build the inverted index")
+
+    tf_parser = subparsers.add_parser(
+        "tf", help="Get term frequency for a given document ID and term"
+    )
+    tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to get frequency for")
 
     args = parser.parse_args()
 
     match args.command:
         case "search":
-            QUERY = args.query.lower().translate(str.maketrans('', '', string.punctuation)).split(' ')
+            QUERY = args.query
             print(f'Searching for: {QUERY}')
-            movies = load_movies()
 
-            # -----------
-            stopWords = load_stop_words()
-
-            results = []
-
-            for movie in movies:
-                clean_title = movie["clean_title"]
-                
-                if check_term(QUERY, clean_title, stopWords):
-                    results.append(movie)
+            results = search_command(QUERY, 5)
             for i, movie in enumerate(results, start=1):
-                if i>5 : break
                 print(f"{i}. {movie['title']}")
         case "build":
             build_command()
+        case "tf":
+            tf = tf_command(args.doc_id, args.term)
+            print(f"Term frequency of '{args.term}' in document '{args.doc_id}': {tf}")
         case _:
             parser.print_help()
-
-def build_command() -> None:
-    idx = InvertedIndex()
-    idx.build()
-    idx.save()
-    docs = idx.get_documents("merida")
-    print(f"First document for token 'merida' = {docs[0]}")
 
 
 if __name__ == "__main__":
