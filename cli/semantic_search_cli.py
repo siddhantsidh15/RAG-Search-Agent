6c65c21b03e1d6f37149c7f8ed0e78c73f203a79
siddhantsiddh15 <siddhant.bitm020@gmail.com>
2026-09-13 20:24:21 +0530

    Embedding models

---
+++ cli/semantic_search_cli.py
@@ -0,0 +1,23 @@
+import argparse
+from lib.semantic_search import verify_model
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    verify_parser = subparsers.add_parser("verify", help="verify the model")
+
+    args = parser.parse_args()
+
+
+    match args.command:
+        case "verify":
+            verify_model()
+        case _:
+            parser.print_help()
+
+
+
+if __name__ == "__main__":
+    main()
