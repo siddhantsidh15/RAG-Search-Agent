56b5cd8ff8d5e7714ee245670c31dbe4d2ddd76c
siddhantsiddh15 <siddhant.bitm020@gmail.com>
2026-09-16 11:12:34 +0530

    Till text embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,14 +1,26 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings : None
+        self.documents : None
+        self.document_map = {}
 
 
     def generate_embedding(self, text: str):
         if not text:
             raise ValueError("Blank message aa gya chuuu")
         return self.model.encode([text])[0]
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        
+        texts = []
+        for document in documents:
+            self.document_map[document["id"]] = document
+            texts.append(f"{document['title']}: {document['description']}")
 
 
 
